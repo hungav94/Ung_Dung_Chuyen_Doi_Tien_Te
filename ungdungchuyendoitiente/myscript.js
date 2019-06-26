ef02adb940bf0e90d0a69e400004d1549a8b1d51
hungavcodegym <anhung94@gmail.com>
2019-06-26 16:12:00 +0700

    update file

--- ungdungchuyendoitiente/myscript.js
+++ ungdungchuyendoitiente/myscript.js
@@ -1,26 +1,26 @@
 function showMessage() {
-    let x = document.getElementById("amount");
-    let fromC = document.getElementById("fromC");
-    let toC = document.getElementById("toC");
+    let x = document.getElementById("amount").value;
+    let fromC = document.getElementById("fromC").value;
+    let toC = document.getElementById("toC").value;
     let result;
-    if (fromC.value == "vnd" && toC.value == "vnd") {
-        result = x.value;
-        document.getElementById("re").innerHTML = result;
+    if (fromC == "vnd" && toC == "vnd") {
+        result = x;
+        document.getElementById("result").innerHTML = result;
         // document.write("result = " + result);
     }
-    if (fromC.value == "vnd" && toC.value == "usd") {
-        result = x.value * 23000;
-        document.getElementById("re").innerHTML = result;
+    if (fromC == "vnd" && toC == "usd") {
+        result = x * 23000;
+        document.getElementById("result").innerHTML = result;
         // document.write("result = " + result);
     }
-    if (fromC.value == "usd" && toC.value == "usd") {
-        result = x.value;
-        document.getElementById("re").innerHTML = result;
+    if (fromC == "usd" && toC == "usd") {
+        result = x;
+        document.getElementById("result").innerHTML = result;
         // document.write("result = " + result);
     }
-    if (fromC.value == "usd" && toC.value == "vnd") {
-        result = x.value / 23000;
-        document.getElementById("re").innerHTML = result;
+    if (fromC == "usd" && toC == "vnd") {
+        result = x / 23000;
+        document.getElementById("result").innerHTML = result;
         // document.write("result = " + result);
     }
 }
